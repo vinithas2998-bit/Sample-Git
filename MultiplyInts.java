599c755dadaf1bd0914228bdc959da6701e67b05
Vinitha S <vinithas2998@gmail.com>
2026-02-01 09:07:53 +0530

    Add MultiplyInts class to perform multiplication of integers

---
+++ MultiplyInts.java
@@ -0,0 +1,24 @@
+public class MultiplyInts {
+
+    /**
+     * Multiplies two integers and returns the product
+     * @param a first integer
+     * @param b second integer
+     * @return product of a and b
+     */
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    /**
+     * Main method demonstrating multiplication
+     */
+    public static void main(String[] args) {
+        MultiplyInts m = new MultiplyInts();
+        int prod1 = m.multiply(4, 5);
+        System.out.println("Product of 4 and 5: " + prod1);
+
+        int prod2 = m.multiply(-3, 7);
+        System.out.println("Product of -3 and 7: " + prod2);
+    }
+}
